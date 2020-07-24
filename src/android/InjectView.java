bffe531c7a39adcb69240e913a6092d28139d4cf
Chris Schmich <schmch@gmail.com>
2020-07-24 13:41:26 -0500

    Android: Concatenate and load all Cordova sources at once. This is the parallel to the iOS changes in 81da18a0a8129ccb3b03fcd9d1aee1d5ebad8f11. Also, we simplify how JS evaluation is done by directly calling CordovaWebViewEngine.evaluateJavascript.

--- src/android/InjectView.java
+++ src/android/InjectView.java
@@ -1,22 +1,16 @@
 package me.steenman.injectview;
 
 import android.util.Log;
-import android.view.View;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.webkit.ValueCallback;
 
-import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaActivity;
 
 import org.json.JSONArray;
-import org.json.JSONObject;
 import org.json.JSONException;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Method;
+import java.lang.StringBuilder;
 import java.util.ArrayList;
 
 public class InjectView extends CordovaPlugin {
@@ -58,55 +52,35 @@ public class InjectView extends CordovaPlugin {
 		return scripts;
 	}
 
-	private void injectJavascriptFiles(ArrayList<String> files) {
-		InjectView injectView = this;
-
-		this.cordova.getThreadPool().execute(new Runnable() {
-			@Override
-			public void run() {
-				for (String file : files) {
-					Log.v(TAG, String.format("Injecting script '%s'", file));
-
-					String content = getFileContents(file);
-					if (content == null) {
-						Log.v(TAG, String.format("Could not load script '%s'", file));
-						continue;
-					}
-					if (content.isEmpty()) {
-						Log.v(TAG, String.format("No contents found in script '%s'", file));
-						return;
-					}
-					final String script = "//# sourceURL=" + file + "\r\n" + content + "\r\nconsole.log('Loaded script "
-							+ file + "');";
-					injectView.activity.runOnUiThread(new Runnable() {
-						@Override
-						public void run() {
-							View webView = injectView.webView.getEngine().getView();
-							ValueCallback<String> resultCallback = null;
-							try {
-								Method evaluateJavascriptMethod = webView.getClass().getMethod("evaluateJavascript",
-										new Class[] { String.class,
-												(Class<ValueCallback<String>>) (Class<?>) ValueCallback.class });
-								evaluateJavascriptMethod.invoke(webView, script, resultCallback);
-							} catch (Exception e) {
-								Log.v(TAG, String.format(
-										"WARNING: Webview does not support 'evaluateJavascript' method. Webview type: '%s'",
-										webView.getClass().getName()));
-								injectView.webView.getEngine().loadUrl("javascript:" + script, false);
-
-								if (resultCallback != null) {
-									resultCallback.onReceiveValue(null);
-								}
-							}
-						}
-					});
+	private void injectJavascriptFiles(ArrayList<String> filenames) {
+		this.cordova.getThreadPool().execute(() -> {
+			// Concatenate all scripts into one large script.
+			StringBuilder script = new StringBuilder();
+			for (String filename : filenames) {
+				String content = getFileContents(filename);
+				if (content == null || content.isEmpty()) {
+					continue;
 				}
+
+				script.append(content);
+				script.append("\n;\n");
 			}
+
+			// Inject scripts by direct evaluation in the WebView.
+			String expression = script.toString();
+			this.activity.runOnUiThread(() -> {
+				try {
+					this.webView.getEngine().evaluateJavascript(expression, null);
+				} catch (Exception e) {
+					Log.e(TAG, "Failed to inject scripts.");
+					e.printStackTrace();
+				}
+			});
 		});
 	}
 
 	private String getFileContents(String filename) {
-		try (InputStream stream = activity.getResources().getAssets().open(filename)) {
+		try (InputStream stream = this.activity.getResources().getAssets().open(filename)) {
 			int size = stream.available();
 			byte[] bytes = new byte[size];
 			stream.read(bytes);
@@ -115,6 +89,6 @@ public class InjectView extends CordovaPlugin {
 			Log.e(TAG, String.format("Failed to read file: %s.", filename));
 		}
 
-		return "";
+		return null;
 	}
 }
