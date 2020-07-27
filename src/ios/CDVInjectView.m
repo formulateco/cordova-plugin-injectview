0487fbc0eab94a572b6baee9184048b7b79c4bb7
Chris Schmich <schmch@gmail.com>
2020-07-27 07:49:52 -0700

    iOS: Change how Cordova plugin scripts are discovered for loading. This corresponds to the Android changes made in 7b8d8582cfe9c45571fafeedbb7274182f782f01.

--- src/ios/CDVInjectView.m
+++ src/ios/CDVInjectView.m
@@ -8,40 +8,38 @@
 }
 
 - (void)pageDidLoad:(NSNotification *)event {
-    // Read cordova.js and cordova_plugins.js.
-    NSMutableArray *scripts = [NSMutableArray array];
-    [scripts addObject:[self readJavaScriptFile:@"www/cordova"]];
-    [scripts addObject:[self readJavaScriptFile:@"www/cordova_plugins"]];
+    NSArray *filenames = [self getCordovaFiles];
+    if (filenames == nil) {
+        return;
+    }
 
-    // Read source for each plugin.
-    for (NSDictionary *pluginParameters in [self parseCordovaPlugins]) {
-        NSString *file = pluginParameters[@"file"];
-        NSString *path = [[NSString stringWithFormat:@"www/%@", file] stringByDeletingPathExtension];
-        [scripts addObject:[self readJavaScriptFile:path]];
+    NSMutableArray *scripts = [NSMutableArray array];
+    for (NSString *filename in filenames) {
+        NSString *content = [self readResourceFile:filename];
+        [scripts addObject:content];
     }
 
     // Evaluate all concatenated sources at once.
-    NSString *js = [scripts componentsJoinedByString:@"\n;\n"];
-    [self.webViewEngine evaluateJavaScript:js completionHandler:^(id result, NSError *err) { }];
+    NSString *expression = [scripts componentsJoinedByString:@"\n;\n"];
+    [self.webViewEngine evaluateJavaScript:expression completionHandler:^(id result, NSError *err) { }];
 }
 
-- (NSString*)readJavaScriptFile:(NSString*)resource {
-    NSString *path = [[NSBundle mainBundle] pathForResource:resource ofType:@"js"];
-    NSString *js = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:NULL];
-    return js;
+- (NSString*)readResourceFile:(NSString*)filename {
+    NSString *path = [[NSBundle mainBundle] pathForResource:filename ofType:nil];
+    return [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:NULL];
 }
 
-- (NSArray*)parseCordovaPlugins {
-    NSString *js = [self readJavaScriptFile:@"www/cordova_plugins"];
-    NSScanner *scanner = [NSScanner scannerWithString:js];
-    [scanner scanUpToString:@"[" intoString:nil];
-    NSString *substring = nil;
-    [scanner scanUpToString:@"];" intoString:&substring];
-    substring = [NSString stringWithFormat:@"%@]", substring];
-    NSError *localError;
-    NSData *data = [substring dataUsingEncoding:NSUTF8StringEncoding];
-    NSArray *pluginObjects = [NSJSONSerialization JSONObjectWithData:data options:0 error:&localError];
-    return pluginObjects;
+- (NSArray*)getCordovaFiles {
+    NSString *manifest = [self readResourceFile:@"www/cordova-plugin-injectview.json"];
+    NSData *data = [manifest dataUsingEncoding:NSUTF8StringEncoding];
+
+    NSError *error;
+    NSArray *filenames = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+    if (filenames == nil) {
+        NSLog(@"Failed to load Cordova script manifest.");
+    }
+
+    return filenames;
 }
 
 @end
